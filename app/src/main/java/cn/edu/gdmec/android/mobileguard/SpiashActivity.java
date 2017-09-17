6ebd7494958b348fd212494c6f4449e38d2b3cc0
zhuge06666 <897126541@qq.com>
2017-09-17 22:25:52 +0800

    Rejected commit: Defaultqq

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SpiashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SpiashActivity.java
@@ -2,12 +2,27 @@ package cn.edu.gdmec.android.mobileguard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import cn.edu.gdmec.android.mobileguard.m1home.utils.MyUtils;
+import cn.edu.gdmec.android.mobileguard.m1home.utils.VersionUpdateUtils;
 
 public class SpiashActivity extends AppCompatActivity {
-
+private TextView mTvVerson;
+    private  String mVerson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spiash);
+        mVerson = MyUtils.getVerson(getApplicationContext());
+        mTvVerson = (TextView)findViewById(R.id.tv_spiash_version);
+        mTvVerson.setText("版本号："+mVerson);
+        final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVerson,SpiashActivity.this);
+        new Thread(){
+            public void run(){
+                super.run();
+                versionUpdateUtils.getCloudVersion();
+            }
+        }.start();
     }
 }
