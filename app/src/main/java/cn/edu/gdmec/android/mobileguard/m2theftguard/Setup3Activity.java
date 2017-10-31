f62d961c238a2182618e10af96532ecf5166a219
zhuge06666 <897126541@qq.com>
2017-10-31 20:49:20 +0800

    手机防盗界面SIM卡绑定和设置安全号码

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup3Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup3Activity.java
@@ -1,4 +1,4 @@
-package cn.edu.gdmec.android.mobileguard.m2theftguard.dialog;
+package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,20 +12,25 @@ import android.widget.Toast;
 import cn.edu.gdmec.android.mobileguard.R;
 
 /**
- * Created by Asus on 2017/10/13.
+ * Created by Administrator on 2017/10/31.
  */
 
 public class Setup3Activity extends BaseSetUpActivity implements View.OnClickListener{
     private EditText mInputPhone;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_3);
+        getSupportActionBar().hide();
         ((RadioButton) findViewById(R.id.rb_third)).setChecked(true);
+        initView();
+    }
+    private void initView() {
+        ((RadioButton)findViewById(R.id.rb_third)).setChecked(true);
         findViewById(R.id.btn_addcontact).setOnClickListener(this);
         mInputPhone=(EditText)findViewById(R.id.et_inputphone);
         String safephone=sp.getString("safephone",null);
-        if(!TextUtils.isEmpty(safephone)){
+        if (!TextUtils.isEmpty(safephone)){
             mInputPhone.setText(safephone);
         }
     }
@@ -33,32 +38,33 @@ public class Setup3Activity extends BaseSetUpActivity implements View.OnClickLis
     public void showNext() {
         String safePhone=mInputPhone.getText().toString().trim();
         if (TextUtils.isEmpty(safePhone)){
-            Toast.makeText(this,"请输入安全号码",Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"请输入安全号码",Toast.LENGTH_SHORT).show();
             return;
         }
         SharedPreferences.Editor edit=sp.edit();
         edit.putString("safephone",safePhone);
         edit.commit();
-         startActivityAndFinishSelf(Setup4Activity.class);
+        startActivityAndFinishSelf(Setup4Activity.class);
     }
 
     @Override
     public void showPre() {
-     startActivityAndFinishSelf(Setup2Activity.class);
+        startActivityAndFinishSelf(Setup2Activity.class);
     }
+
     @Override
-    public void onClick(View view){
-     switch (view.getId()){
-         case R.id.btn_addcontact:
-             startActivityForResult(new Intent(this, ContactSelectActivity.class),0);
-             break;
-     }
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_addcontact:
+                startActivityForResult(new Intent(this,ContactSelectActivity.class),0);
+                break;
+        }
     }
     @Override
     protected void onActivityResult(int requestCode,int resultCode,Intent data){
         super.onActivityResult(requestCode,resultCode,data);
-        if(data!=null){
-            String phone = data.getStringExtra("phone");
+        if (data!=null){
+            String phone =data.getStringExtra("phone");
             mInputPhone.setText(phone);
         }
     }
